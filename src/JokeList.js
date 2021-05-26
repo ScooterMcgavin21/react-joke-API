eab88702d14a8b77f4c0d166060181b0d738bfd5
Scooter <scott.skrobel@gmail.com>
2021-05-26 14:33:50 -0600

    fetched new jokes from API

--- src/JokeList.js
+++ src/JokeList.js
@@ -5,18 +5,34 @@ class JokeList extends Component {
   static defaultProps = {
     getJoke: 10
   };
+  /** initalize state */
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+
   async componentDidMount() {
-    // Load jokes via axios
-    let response = await axios.get("https://icanhazdadjoke.com/", {
-      headers: { Accept: "application/json" }
-    });
-    console.log(response.data.joke);
+    // Load jokes via axios | want json format not html
+    let jokes = [];
+    while (jokes.length < this.props.getJoke) {
+      let response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" }
+      });
+      jokes.push(response.data.joke)
+    }
+    this.setState({ jokes: jokes });
+    //console.log(jokes);
   }
 
   render() {
     return (
-      <div>
+      <div className='JokeList'>
         <h1>Joke List</h1>
+        <div className='JokeList-jokes'>
+          {this.state.jokes.map(j => (
+            <div>{j}</div>
+          ))}
+        </div>
       </div>
     );
   }
